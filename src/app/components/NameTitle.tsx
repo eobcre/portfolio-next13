78766b87891dbb5a4de5f10cd8b11b158d786109
Emmy Manning <eobcre@gmail.com>
2023-11-14 14:25:23 -0500

    updated text bold

--- src/app/components/NameTitle.tsx
+++ src/app/components/NameTitle.tsx
@@ -5,7 +5,7 @@ type NameTitleProps = {
 const NameTitle: React.FC<NameTitleProps> = ({ text }) => {
   return (
     <>
-      <span className='text-[#303030] text-2xl p-4 md:text-5xl'>{text}</span>
+      <span className='text-[#303030] text-2xl font-bold p-4 md:text-5xl'>{text}</span>
     </>
   );
 };
