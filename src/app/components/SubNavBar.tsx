a73fcb83d708cb087e4b54e22230ac035769e106
Emmy Manning <eobcre@gmail.com>
2023-11-14 11:24:58 -0500

    replaced data in functional component

--- src/app/components/SubNavBar.tsx
+++ src/app/components/SubNavBar.tsx
@@ -3,35 +3,35 @@ import { nanoid } from 'nanoid';
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
 import { HiOutlineMail } from 'react-icons/hi';
 
-const subNavMenuData = [
-  {
-    id: nanoid(),
-    icon: (
-      <BsLinkedin className='text-white text-3xl hover:text-[#97dffc] duration-500' />
-    ),
-    href: 'https://www.linkedin.com/in/emmymanning',
-    target: '_blank',
-    rel: 'noopener noreferrer',
-  },
-  {
-    id: nanoid(),
-    icon: (
-      <BsGithub className='text-white text-3xl hover:text-[#97dffc] duration-500' />
-    ),
-    href: 'https://github.com/eobcre',
-    target: '_blank',
-    rel: 'noopener noreferrer',
-  },
-  {
-    id: nanoid(),
-    icon: (
-      <HiOutlineMail className='text-white text-4xl hover:text-[#97dffc] duration-500' />
-    ),
-    href: 'mailto:eobcre@gmail.com',
-  },
-];
-
 const SubNavBar = () => {
+  const subNavMenuData = [
+    {
+      id: nanoid(),
+      icon: (
+        <BsLinkedin className='text-white text-3xl hover:text-[#97dffc] duration-500' />
+      ),
+      href: 'https://www.linkedin.com/in/emmymanning',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
+    {
+      id: nanoid(),
+      icon: (
+        <BsGithub className='text-white text-3xl hover:text-[#97dffc] duration-500' />
+      ),
+      href: 'https://github.com/eobcre',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
+    {
+      id: nanoid(),
+      icon: (
+        <HiOutlineMail className='text-white text-4xl hover:text-[#97dffc] duration-500' />
+      ),
+      href: 'mailto:eobcre@gmail.com',
+    },
+  ];
+
   return (
     <nav>
       <ul className='flex gap-5 p-4'>
