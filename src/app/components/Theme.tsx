963b881830d14ee40c2d661ae3972db34abe56a5
Emmy Manning <eobcre@gmail.com>
2023-11-22 11:40:25 -0500

    updated breakpoints

---
+++ src/app/components/Theme.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+// import { useTheme } from 'next-themes';
+
+const Theme = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Theme;
--- src/app/components/TypeWritter.tsx
+++ src/app/components/TypeWritter.tsx
@@ -33,7 +33,7 @@ const TypeWritter: React.FC<TypeWritterProps> = ({ text, delay }) => {
 
   return (
     <>
-      <span className='custom-text text-4xl font-bold p-4 min-[984px]:text-7xl'>
+      <span className='custom-text text-4xl font-bold p-4 min-[970px]:text-7xl'>
         {currentText}
         <span className={isCursorVisible ? 'cursor' : ''}>|</span>
       </span>
