8a89374ad82a3e3e41f5b66baa27e0a2eedc9f99
Emmy Manning <eobcre@gmail.com>
2023-11-26 18:37:50 -0500

    updated padding

--- src/app/components/Description.tsx
+++ src/app/components/Description.tsx
@@ -1,6 +1,6 @@
 const Description = () => {
   return (
-    <div className='text-[#333333] text-7xl font-bold p-4'>
+    <div className='text-[#333333] text-7xl font-bold py-4'>
       <p className="mb-2">Dive into Creativity</p>
       <p>Lorem Lorem Lorem Lorem</p>
     </div>
