3871d6a6787030ffa1d6919955ac129773f6925b
Emmy Manning <eobcre@gmail.com>
2023-12-03 20:30:50 -0500

    added a new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // components
 import HeroSection from './components/HeroSection';
+import Latest from './components/Latest';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Latest />
     </main>
   );
 }
