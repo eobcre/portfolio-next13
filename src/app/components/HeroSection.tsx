c8ca28aa78207dd7eeb3a38887354d404eca662d
Emmy Manning <eobcre@gmail.com>
2023-12-03 20:33:27 -0500

    removed components and updated details

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -3,11 +3,9 @@
 import { useState, useEffect } from 'react';
 
 // components
-import NameTitle from './NameTitle';
 import TypeWritter from './TypeWritter';
 import TypeWritterMobile from './TypeWritterMobile';
 import SubNavBar from './SubNavBar';
-import Description from './Description';
 
 const HeroSection = () => {
   const [isFrontendFinished, setIsFrontendFinished] = useState(false);
@@ -21,14 +19,24 @@ const HeroSection = () => {
 
   return (
     <div className='flex flex-col pl-4 w-full'>
-      <NameTitle text="Hi, I'm Emmy." />
-      <TypeWritter text='Frontend Developer.' delay={200} />
+      <h1 className='text-white text-4xl font-bold py-4 xs:text-5xl sm:text-5xl md:text-6xl'>
+        Hi, I'm Emmy.
+      </h1>
+      <TypeWritter
+        text='Frontend Developer .'
+        delay={200}
+        className='hidden pre-md:block custom-text font-bold py-4 pre-md:text-7xl md:text-[5rem] lg:text-8xl 2xl:text-[7rem] 4xl:text-[12rem]'
+      />
       <TypeWritterMobile text='Frontend' delay={200} />
       {isFrontendFinished && (
         <TypeWritterMobile text='Developer.' delay={200} />
       )}
       <SubNavBar />
-      <Description />
+      <div className='text-[#333333] text-7xl font-bold py-4'>
+        <p className='mb-2'>Dive into Creativity</p>
+        <p className='mb-2'>Lorem Lorem Lorem Lorem</p>
+        <p>Lorem Lorem Lorem Lorem</p>
+      </div>
     </div>
   );
 };
