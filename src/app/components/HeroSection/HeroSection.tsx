e4fc1a3d69bb35fc70637f0bedfc4e02bf0af056
Emmy Manning <eobcre@gmail.com>
2024-01-06 22:12:29 -0500

    updated responsive font size

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 
 // components
-import TypeWriting from '../TypeWriting';
+// import TypeWriting from '../TypeWriting';
 import TypeWritingMobile from '../TypeWritingMobile';
 import IconBar from './IconBar';
 
@@ -18,22 +18,20 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <div className='flex flex-col justify-center pl-4 w-full h-screen'>
-      <h1 className='text-white text-4xl font-bold py-4 xs:text-5xl sm:text-5xl md:text-6xl'>
+    <div className='flex flex-col justify-center pl-4 py-4 w-full'>
+      <h1 className='text-white text-4xl font-bold py-4 sm:text-[5vw]'>
         Hi, I'm Emmy.
       </h1>
-      <TypeWriting
+      {/* <TypeWriting
         text='Frontend Developer .'
         delay={200}
         className='hidden pre-md:block custom-text font-bold py-4 pre-md:text-7xl md:text-[5rem] lg:text-8xl 2xl:text-[7rem] 4xl:text-[12rem]'
-      />
+      /> */}
       <TypeWritingMobile text='Frontend' delay={200} />
       {isFrontendFinished && (
         <TypeWritingMobile text='Developer.' delay={200} />
       )}
       <IconBar />
-      <div className='text-[#333333] text-7xl font-bold py-4'>
-      </div>
     </div>
   );
 };
