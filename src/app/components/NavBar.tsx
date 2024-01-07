e4d9c2a8ff80631c3b83e8a3851829782105560e
Emmy Manning <eobcre@gmail.com>
2024-01-07 01:26:51 -0500

    updated nav style

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -45,12 +45,12 @@ const NavBar = () => {
 
   return (
     <nav className='fixed md:top-1/2 md:transform md:-translate-y-1/2 right-0 p-4'>
-      <div className='text-right md:hidden'>
+      <div className='text-right pt-2 md:hidden'>
         <button onClick={toggleNav}>
           {navbar ? (
-            <RiCloseLine className='text-white text-4xl border-2 border-white rounded-full hover:rotate-[360deg] hover:duration-1000 w-[60px] h-[60px] p-2' />
+            <RiCloseLine className='text-white border-2 border-white rounded-full hover:rotate-[360deg] hover:duration-1000 w-[50px] h-[50px] p-2' />
           ) : (
-            <RiMenu3Line className='text-white text-4xl border-2 border-white rounded-full hover:rotate-[360deg] hover:duration-1000 w-[60px] h-[60px] p-3' />
+            <RiMenu3Line className='text-white border-2 border-white rounded-full hover:rotate-[360deg] hover:duration-1000 w-[50px] h-[50px] p-3' />
           )}
         </button>
       </div>
@@ -59,8 +59,9 @@ const NavBar = () => {
         <ul className='flex flex-col'>
           {navMenuData.map((menu) => (
             <Link key={menu.id} href={menu.href}>
-              <li onClick={closeNav}
-                className={`flex justify-center items-center text-white font-bold tracking-wide rounded-full m-2 px-6 py-2 hover:rotate-[360deg] hover:duration-1000 cursor-pointer w-[100px] h-[100px] md:text-lg md:m-3 ${menu.background}`}
+              <li
+                onClick={closeNav}
+                className={`flex justify-center items-center text-white text-md font-bold uppercase tracking-wide rounded-full m-2 px-6 py-2 hover:rotate-[360deg] hover:duration-1000 cursor-pointer w-[100px] h-[100px] md:m-3 ${menu.background}`}
               >
                 {menu.name}
               </li>
