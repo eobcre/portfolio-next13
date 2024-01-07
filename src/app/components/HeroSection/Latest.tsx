6faa3a5d8e969ac6436a245ced1d0bc30ec9c4c3
Emmy Manning <eobcre@gmail.com>
2024-01-06 20:55:47 -0500

    updated title

--- src/app/components/HeroSection/Latest.tsx
+++ src/app/components/HeroSection/Latest.tsx
@@ -5,7 +5,7 @@ import ScrollObserver from '../ScrollObserver';
 const Latest = () => {
   return (
     <div className='px-10 py-20 md:pl-10 md:pr-48'>
-      <ScrollObserver title="What's New .">
+      <ScrollObserver title="News Feed .">
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 pre-md:-mt-5 md:-mt-5 lg:-mt-6 '>
           <p className='text-white'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
