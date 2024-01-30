c67f4e691bd17c782866e2966a717fac49cf594a
Emmy Manning <emswoo@EM.local>
2024-01-30 14:39:54 -0500

    updated layout

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -5,10 +5,19 @@ import ScrollObserver from '@/app/components/ScrollObserver';
 
 const ContactPage = () => {
   return (
-    <div className='pl-10 py-10'>
+    <div className='px-10 py-20 md:pl-10 md:pr-48'>
       <ScrollObserver title='Contact.'>
         <></>
       </ScrollObserver>
+      <div>
+        <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Say Hello.</h1>
+        <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In ea quae
+          maxime dolorum, non velit impedit dolorem quisquam, dolores quidem
+          repellendus! Labore eos ex autem commodi nesciunt veritatis similique
+          assumenda!
+        </div>
+      </div>
     </div>
   );
 };
