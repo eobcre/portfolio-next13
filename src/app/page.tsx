431e2c6aafce51ef2d4632b5c909e5c5e0cdf52b
Emmy Manning <emswoo@EM.local>
2024-01-31 10:08:16 -0500

    added Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // components
 import HeroSection from './components/HeroSection/HeroSection';
 import Latest from './components/HeroSection/Latest';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <Latest />
+      <Footer />
     </main>
   );
 }
