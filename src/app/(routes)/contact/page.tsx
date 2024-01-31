3633b48d7f2e4053d9faeeebb267584ab2caa3d8
Emmy Manning <emswoo@EM.local>
2024-01-31 00:33:40 -0500

    updated contact page

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -12,28 +12,6 @@ const ContactPage = () => {
           Say Hello.
         </h1>
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            placeholder='Name'
-            className='rounded-md outline-none mt-10 p-4 w-full'
-          />
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='Email'
-            className='rounded-md outline-none mt-6 p-4 w-full'
-          />
-          <textarea
-            name=''
-            id=''
-            cols={30}
-            rows={10}
-            placeholder='Message'
-            className='rounded-md outline-none resize-none mt-6 p-4 w-full'
-          ></textarea>
         </div>
       </div>
     </div>
