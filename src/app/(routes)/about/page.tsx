8011dfc07d654e24572dfd4aab4c4d4b391f06ff
Emmy Manning <emswoo@EM.local>
2024-01-31 00:08:21 -0500

    created skills and experience sections

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 
 // components
-import ScrollObserver from '@/app/components/ScrollObserver';
+import TypeWriting from '@/app/components/TypeWriting';
 
 const AboutPage = () => {
   return (
-    <div className='pl-10 py-10'>
-      <ScrollObserver title='About.'>
-        <></>
-      </ScrollObserver>
+    <div className='px-10 py-20 md:pl-10 w-full md:pr-48'>
+      <TypeWriting text='About.' delay={200} />
+      <div className='pt-20'>
+        <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Skills</h1>
+        <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
+        </div>
+      </div>
+      <div className='pt-20'>
+        <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Experience</h1>
+        <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
+        </div>
+      </div>
     </div>
   );
 };
