765173af7b865d064e4c6122dea1571ad463da15
Emmy Manning <emswoo@EM.local>
2024-01-31 16:08:33 -0500

    vercel test

--- src/app/components/HeroSection/Latest.tsx
+++ src/app/components/HeroSection/Latest.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Latest = () => {
   return (
     <div className='px-10 py-20 md:pl-10 md:pr-48'>
-      <div>
+      <div className='xl:px-36'>
         <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Repos</h1>
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
           <p className='text-white'></p>
