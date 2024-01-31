0fe75015ab8ebe93e21e2f2aa886131422a1ab1c
Emmy Manning <emswoo@EM.local>
2024-01-31 14:52:38 -0500

    updated section layout

--- src/app/(routes)/gallery/page.tsx
+++ src/app/(routes)/gallery/page.tsx
@@ -7,7 +7,7 @@ const GalleryPage = () => {
   return (
     <div className='px-10 py-20 md:pl-10 w-full md:pr-48'>
       <TypeWriting text='Gallery.' delay={200} />
-      <div className='pt-28'>
+      <div className='pt-28 xl:px-36'>
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'></div>
       </div>
     </div>
