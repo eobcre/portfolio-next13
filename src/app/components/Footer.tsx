2adf84767746cd6e58f7dfb4d7ab7c498298bf21
Emmy Manning <emswoo@EM.local>
2024-01-31 10:07:18 -0500

    updated layout and style

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>&copy; {new Date().getFullYear()} Emmy Manning</div>;
+  return (
+    <div className="px-6 py-10">
+      <p className="text-white text-sm">&copy; {new Date().getFullYear()} Emmy Manning All Rights Reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
