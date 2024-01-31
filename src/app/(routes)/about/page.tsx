bcf955beb064bc7af9568b11ef49286f47a84442
Emmy Manning <emswoo@EM.local>
2024-01-31 13:26:51 -0500

    updated section

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -8,6 +8,14 @@ const AboutPage = () => {
     <div className='px-10 py-20 md:pl-10 w-full md:pr-48'>
       <TypeWriting text='About.' delay={200} />
       <div className='pt-20'>
+      <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Me.</h1>
+        <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
+          <div className='p-14'>
+            <p className='text-gray-300'></p>
+          </div>
+        </div>
+      </div>
+      <div className='pt-20'>
         <h1 className='custom-text text-5xl font-bold sm:text-[8vw]'>Skills</h1>
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'></div>
       </div>
