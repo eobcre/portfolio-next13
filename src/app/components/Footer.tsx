5be353276ed4f688463f656c8b1a34c35217a238
Emmy Manning <emswoo@EM.local>
2024-02-01 14:53:15 -0500

    updated data file name

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,17 +1,8 @@
 import Link from 'next/link';
-import { getFooterNavData } from '../data/data';
-import { getFooterIconData } from '../data/data';
-import { ReactElement } from 'react';
+import { getFooterNavData } from '../data/footerData';
+import { getFooterIconData } from '../data/footerData';
 
-interface FooterIconType {
-  id: string;
-  icon: ReactElement;
-  href: string;
-  target?: string;
-  rel?: string;
-}
-
-const Footer: React.FC<FooterIconType> = () => {
+const Footer = () => {
   return (
     <footer className='bg-[#0D1225] flex items-center text-gray-300 text-sm px-6 py-10 w-full'>
       <p className='mr-10'>
@@ -25,7 +16,7 @@ const Footer: React.FC<FooterIconType> = () => {
         ))}
       </div>
       <div className='flex gap-4'>
-        {getFooterIconData().map((icon: FooterIconType) => (
+        {getFooterIconData().map((icon) => (
           <Link
             key={icon.id}
             href={icon.href}
