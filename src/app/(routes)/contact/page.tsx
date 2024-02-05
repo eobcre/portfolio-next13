a4d7f9dc34b4cf8144a20e8b4e1a0d185889252e
Emmy Manning <emswoo@EM.local>
2024-02-05 14:27:20 -0500

    implemented button component and style

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -2,19 +2,18 @@
 
 // components
 import TypeWriting from '@/app/components/TypeWriting';
+import Button from '@/app/components/Button';
 
 const ContactPage = () => {
-  const h1Style = 'custom-text text-5xl font-bold sm:text-[8vw]'
+  const h1Style = 'custom-text text-5xl font-bold sm:text-[8vw]';
   const inputStyle = 'rounded-md outline-none p-4 w-full';
-  const textareaStyle = 'rounded-md outline-none resize-none mt-6 p-4 w-full'
+  const textareaStyle = 'rounded-md outline-none resize-none mt-6 p-4 w-full';
 
   return (
     <div className='px-10 py-20 w-full min-h-screen md:pl-10 md:pr-48'>
       <TypeWriting text='Contact.' delay={200} />
       <div className='pt-20 xl:px-36'>
-        <h1 className={h1Style}>
-          Say Hello.
-        </h1>
+        <h1 className={h1Style}>Say Hello.</h1>
         <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
           <input
             type='text'
@@ -38,6 +37,7 @@ const ContactPage = () => {
             placeholder='Message'
             className={textareaStyle}
           ></textarea>
+          <Button buttonName='Send' className='custom-text text-lg font-bold border border-blue-300 rounded-md mt-5 px-6 py-3 hover:text-[#97dffc] hover:duration-300' />
         </div>
       </div>
     </div>
