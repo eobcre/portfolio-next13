a27d2d1d8735955b48905e4690cf3f33ccb134bc
Emmy Manning <emswoo@EM.local>
2024-02-05 14:27:41 -0500

    updated className prop

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,9 +1,10 @@
 type ButtonProps = {
   buttonName: string;
+  className: string;
 };
 
-const Button: React.FC<ButtonProps> = ({ buttonName }) => {
-  return <button>{buttonName}</button>;
+const Button: React.FC<ButtonProps> = ({ buttonName, className }) => {
+  return <button className={className}>{buttonName}</button>;
 };
 
 export default Button;
