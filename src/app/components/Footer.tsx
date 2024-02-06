5862f7cce382ceb2c51ff1b0c9d4f44615b50b73
Emmy Manning <emswoo@EM.local>
2024-02-06 18:34:29 -0500

    fixed layout all screens

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,28 +4,34 @@ import { getFooterIconData } from '../data/data';
 
 const Footer = () => {
   return (
-    <footer className='bg-[#0D1225] flex items-center text-gray-300 text-sm px-6 py-10 w-full'>
-      <p className='mr-10'>
-        &copy; {new Date().getFullYear()} Emmy Manning All Rights Reserved.
-      </p>
-      <div className='flex gap-5 mr-10'>
-        {getFooterNavData().map((nav) => (
-          <Link key={nav.id} href={nav.href} className={nav.style}>
-            <p>{nav.name}</p>
-          </Link>
-        ))}
-      </div>
-      <div className='flex gap-4'>
-        {getFooterIconData().map((icon) => (
-          <Link
-            key={icon.id}
-            href={icon.href}
-            target={icon.target}
-            rel={icon.rel}
-          >
-            {icon.icon}
-          </Link>
-        ))}
+    <footer className='bg-[#0D1225] flex justify-center text-gray-300 text-sm px-6 py-10 w-full'>
+      <div className='flex flex-col sm:flex-row'>
+        <div className='mb-1'>
+          <p className='mr-10'>
+            &copy; {new Date().getFullYear()} Emmy Manning All Rights Reserved.
+          </p>
+        </div>
+        <div className='sm:flex'>
+          <div className='flex gap-5 mb-2 mr-10'>
+            {getFooterNavData().map((nav) => (
+              <Link key={nav.id} href={nav.href} className={nav.style}>
+                <p>{nav.name}</p>
+              </Link>
+            ))}
+          </div>
+          <div className='flex gap-4'>
+            {getFooterIconData().map((icon) => (
+              <Link
+                key={icon.id}
+                href={icon.href}
+                target={icon.target}
+                rel={icon.rel}
+              >
+                {icon.icon}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   );
