6b371dad0259695018a0969a325026c0897d3a70
Emmy Manning <emswoo@EM.local>
2024-02-10 20:49:12 -0500

    updated validation in contact form

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -1,10 +1,32 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
+
 // components
 import TypeWriting from '@/app/components/TypeWriting';
 import Button from '@/app/components/Button';
 
+type Form = {
+  name: string;
+  email: string;
+  message: string;
+};
+
 const ContactPage = () => {
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>({
+    mode: 'onChange',
+  });
+
+  const onSubmit = (data: Form) => {
+    console.log(data);
+  };
+
+  // style
   const h1Style = 'custom-text text-5xl font-bold sm:text-[8vw]';
   const inputStyle = 'rounded-md outline-none p-4 w-full';
   const textareaStyle = 'rounded-md outline-none resize-none mt-6 p-4 w-full';
@@ -14,31 +36,52 @@ const ContactPage = () => {
       <TypeWriting text='Contact.' delay={200} />
       <div className='pt-20 xl:px-36'>
         <h1 className={h1Style}>Say Hello.</h1>
-        <div className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='bg-[#333333] bg-opacity-50 rounded-md -mt-3 sm:-mt-4 p-10 md:-mt-5 lg:-mt-6'
+        >
           <input
             type='text'
             id='name'
-            name='name'
             placeholder='Name'
             className={`mt-10 ${inputStyle}`}
+            {...register('name', {
+              required: 'Please enter your name.',
+            })}
           />
+          <p className='text-gray-500 mt-2'>
+            {errors.name?.message as React.ReactNode}
+          </p>
           <input
             type='email'
             id='email'
-            name='email'
             placeholder='Email'
             className={`mt-6 ${inputStyle}`}
+            {...register('email', {
+              required: 'Please enter your email.',
+            })}
           />
+          <p className='text-gray-500 mt-2'>
+            {errors.email?.message as React.ReactNode}
+          </p>
           <textarea
-            name=''
-            id=''
+            id='message'
             cols={30}
             rows={10}
             placeholder='Message'
             className={textareaStyle}
+            {...register('message', {
+              required: 'Please enter your message.',
+            })}
           ></textarea>
-          <Button buttonName='Send' className='custom-text text-lg font-bold border border-blue-300 rounded-md mt-5 px-6 py-3 hover:text-[#97dffc] hover:duration-300' />
-        </div>
+          <p className='text-gray-500 mt-2'>
+            {errors.message?.message as React.ReactNode}
+          </p>
+          <Button
+            buttonName='Send'
+            className='custom-text text-lg font-bold border border-blue-300 rounded-md mt-5 px-6 py-3 hover:text-[#97dffc] hover:duration-300'
+          />
+        </form>
       </div>
     </div>
   );
